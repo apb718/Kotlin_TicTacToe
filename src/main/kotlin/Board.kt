f1f51a7efbf434628c999cb7c75691d07470c8c5
apb718 <apb718@gmail.com>
2022-04-03 15:00:23 -0500

    Final Commit.  All should be working

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 class Board(private val n: Int) {
 
     private val empty = "___"
@@ -24,16 +26,82 @@ class Board(private val n: Int) {
     }
 
     fun placePiece(x: Int, y: Int, move: String) {
-        if (!isGameOver &&
-            board[x][y] == empty
-            && isPositionValid(x,y) == true){
+        if (!isGameOver && isPositionValid(x,y) == true && board[x][y] == empty ){
             moveCount++
             board[x][y] = "_" + move + "_"
             printBoard()
+            isGameOver = isWinningMove(x,y,move) || isDraw()
+            if(isGameOver && !isDraw()){
+                println("Winner winner chicken dinner")
+            }
+            else if(isDraw()){
+                println("Draw")
+            }
         }
     }
 
     fun isPositionValid(x: Int, y: Int): Boolean {
         return ((x in 0 until n) && (y in 0 until n))
+    }
+
+    fun resetGame() {
+        resetBoard()
+        moveCount = 0
+        isGameOver = false
+    }
+
+    fun isDraw(): Boolean {
+        return (moveCount == n.toDouble().pow(2).toInt())
+    }
+
+    private fun isWinningMove(x: Int, y: Int, move: String): Boolean {
+        //check the row
+        for(i in 0 until n) {
+//            println(i);
+            if (board[x][i] != "_" + move + "_"){
+                break
+            }
+            if (i == n-1) {
+//                println("Winning By Horizontal")
+                return true
+            }
+        }
+        // check col
+        for(i in 0 until n) {
+            if (board[i][y] != "_" + move + "_"){
+                break
+            }
+            if(i ==n-1) {
+//                println("Winning By Vert")
+                return true
+            }
+        }
+
+        //check diagonal
+        if(x == y) {
+            for (i in 0 until n) {
+                if (board[i][i] != "_" + move + "_") {
+                    break
+                }
+                if(i == n-1){
+//                    println("Winning By Diag1")
+                    return true
+                }
+            }
+        }
+        //check other diagonal
+        if( x + y == n - 1){
+            for(i in 0 until n ) {
+                if(board[i][(n-1)-i] != "_" + move + "_"){
+                    break
+                }
+                if( i == n-1){
+//                    println("Winning By Diag2")
+                    return true
+                }
+            }
+        }
+//        println("Returning False")
+        return false
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,59 @@
+import java.util.*
 
 private var board = Board(0)
+private val player1 ="X"
+private val player2 = "O"
+private var currentPlayer = ""
+private var answer = ""
+
 fun main() {
-    board = Board(3)
+    val input = Scanner(System.`in`)
+
+    println("Please enter the size of your board")
+    val size = Integer.parseInt(input.nextLine())
+    board = Board(size)
     board.printBoard()
-    board.placePiece(0,0,"X")
+
+    while(!board.isGameOver){
+        takeTurns()
+        println("$currentPlayer's turn")
+        println("Enter Row Number:")
+        val row = Integer.parseInt(input.nextLine()) - 1
+        println("Enter Column Number:")
+        val col = Integer.parseInt(input.nextLine()) - 1
+
+        board.placePiece(row,col,currentPlayer)
+
+        if(board.isGameOver) {
+            println("Do you want to play again? Type y or yes")
+            answer = input.nextLine()
+            if(isPlayingAgain(answer)){
+                board.resetGame()
+                for(i in 0 until 100){
+                    println()
+                }
+                board.printBoard()
+            }
+            else {
+                println("Thank you for playing Tic Tac Toe!")
+            }
+        }
+
+    }
+
+
+}
+
+fun takeTurns() {
+    currentPlayer = if(player1 == currentPlayer){
+        player2
+    }
+    else {
+        player1
+    }
+}
+
+fun isPlayingAgain(answer: String): Boolean {
+    return (answer.equals("y", ignoreCase = true)
+            || answer.equals("yes", ignoreCase = true))
 }
